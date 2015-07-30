7d8d57fea02e393ec9b3b64eab254a52871ba031
Johnny A. Solbu <solbu@users.sourceforge.net>
2015-07-30 14:21:33 +0200

    Ny version: 2.0.7

--- finfaktura/__init__.py
+++ finfaktura/__init__.py
@@ -8,7 +8,7 @@
 # $Id$
 ###########################################################################
 
-__version__ = "2.0.6"
+__version__ = "2.0.7"
 
 __all__ = ['fakturabibliotek','oppgradering','f60','cli', 'sikkerhetskopi', 'okonomi', 'epost', 'historikk', 'ekstra', 'fakturafeil', 'fakturakomponenter', 'rapport', 'gui']
 

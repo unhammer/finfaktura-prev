1a6fd024e9895909cadaacbc3ee287ecab531e3c
Johnny A. Solbu <solbu@users.sourceforge.net>
2015-08-04 00:53:33 +0200

    Vi bruker Git, ikke svn.

--- faktura.py
+++ faktura.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
             sys.exit(finfaktura.gui.start())
         except RessurserManglerFeil, (e):
             print e
-            print u"Feil! Ressursene er ikke generert (det må \ndu gjøre dersom du sjekker ut fra SVN selv)"
+            print u"Feil! Ressursene er ikke generert (det må \ndu gjøre dersom du sjekker ut fra Git selv)"
             print u"Lager ressursene. Ta en titt på \nscripts/forbered_ressurser.sh eller \nfinfaktura/ekstra.py for detaljer"
             from finfaktura.ekstra import forbered_ressurser
             forbered_ressurser()
